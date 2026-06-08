12a3a0b6b45d45c3dabcd5bd86144e22a6a3e475
OkashiKami <zbigmoney305@gmail.com>
2026-06-08 14:03:33 -0500

    feat: configure system tray icon to support PORTAL_URL

--- app/src/tray.py
+++ app/src/tray.py
@@ -52,7 +52,8 @@ def setup_tray():
         return
 
     host, port = get_server_address()
-    app_url = f"http://{host}:{port}"
+    portal_url = os.environ.get("PORTAL_URL")
+    app_url = portal_url or f"http://{host}:{port}"
 
     def get_icon_image():
         # Try to load custom override icon if present (excluding the screenshot docs/shirabe.jpg)
@@ -117,11 +118,16 @@ def setup_tray():
                 os._exit(0)
 
     try:
-        menu = pystray.Menu(
+        menu_items = [
             pystray.MenuItem("Open Shirabe", on_clicked, default=True),
+        ]
+        if portal_url:
+            menu_items.append(pystray.MenuItem(f"Portal: {portal_url}", lambda: None, enabled=False))
+        menu_items.extend([
             pystray.MenuItem(f"Running on port {port}", lambda: None, enabled=False),
             pystray.MenuItem("Exit", on_clicked)
-        )
+        ])
+        menu = pystray.Menu(*menu_items)
         
         _tray_icon = pystray.Icon(
             "shirabe",
